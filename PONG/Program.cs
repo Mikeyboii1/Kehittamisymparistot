eb358b18c4b694d9d63ec8a612d762bc99632ffd
Mikko Kirjalainen <s2401170@edu.bc.fi>
2026-04-15 14:33:54 +0300

    palkki-1 tehty

--- PONG/Program.cs
+++ PONG/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace PONG
+﻿using System.Numerics;
+using Raylib_cs;
+using Color = Raylib_cs.Color;
+
+namespace PONG
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Raylib.InitWindow(800, 800, "screensaver");
+            Raylib.SetTargetFPS(30);
+            Vector2 player1 = new Vector2(50, 400);
+            while (Raylib.WindowShouldClose() == false)
+            {
+                float speed = 80.0f;
+                float deltaTime = Raylib.GetFrameTime();
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.Black);
+
+                // Piirrä vasemman puoleinen maila.
+                Raylib.DrawRectangleV(player1, new Vector2(20, 100), Color.White);
+
+
+                //Piitträ oikean puoleinen maila.
+
+
+                //liikuta mailoja ylös ja alas.
+                if (Raylib.IsKeyDown(KeyboardKey.Up))
+                {
+                    player1.Y = player1.Y - speed * Raylib.GetFrameTime();
+                }
+                else if (Raylib.IsKeyDown(KeyboardKey.Down))
+                {
+                    player1.Y = player1.Y + speed * Raylib.GetFrameTime();
+                }
+
+
+                // Piirrä pallo.
+
+                Raylib.EndDrawing();
+            }
         }
     }
 }
