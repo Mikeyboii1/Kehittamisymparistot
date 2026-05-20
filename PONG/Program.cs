e1cd9db8cea118f586d09bc83195ed0dd837a5b4
Mikko Kirjalainen <s2401170@edu.bc.fi>
2026-05-20 22:41:24 +0300

    Ball borders, score board, speed etc.

--- PONG/Program.cs
+++ PONG/Program.cs
@@ -6,109 +6,216 @@ namespace PONG
 {
     internal class Program
     {
+        // Pelaajien sijainnit
+        Vector2 player1;
+        Vector2 player2;
+
+        // Pelaajien pisteet
+        int player1Score = 0;
+        int player2Score = 0;
+
+        // Mailan koko ja nopeus
+        Vector2 playerSize = new Vector2(20, 100);
+        float playerSpeed = 500;
+
+        // Pallon tiedot
+        Vector2 ballPosition;
+        Vector2 ballDirection;
+        float ballSpeed = 400;
+
+        // Ruudun koko
+        int screenWidth = 800;
+        int screenHeight = 800;
+
         static void Main(string[] args)
         {
-            Raylib.InitWindow(800, 800, "screensaver");
+            Program pong = new Program();
+            pong.RunGame();
+        }
+
+        void RunGame()
+        {
+            Raylib.InitWindow(screenWidth, screenHeight, "PONG");
             Raylib.SetTargetFPS(60);
-            Vector2 player2 = new Vector2(700, 400);
-            Vector2 player1 = new Vector2(50, 400);
-            float speed = 1000.0f;
+
+            InitializeGame();
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Update();
+                Draw();
+            }
+
+            Raylib.CloseWindow();
+        }
+
+        void InitializeGame()
+        {
+            int playerToWall = 60;
+
+            // Pelaajien paikat
+            player1 = new Vector2(
+                playerToWall,
+                screenHeight / 2 - playerSize.Y / 2
+            );
+
+            player2 = new Vector2(
+                screenWidth - playerSize.X - playerToWall,
+                screenHeight / 2 - playerSize.Y / 2
+            );
+
+            ResetBall();
+        }
+
+        void ResetBall()
+        {
+            // Pallo keskelle
+            ballPosition = Raylib.GetScreenCenter();
+
+            // Pallon suunta
+            ballDirection = Vector2.Normalize(new Vector2(1, 0.5f));
+        }
+
+        void Update()
+        {
+            MovePlayers();
+            MoveBall();
+            CheckBallCollisions();
+        }
+
+        void MovePlayers()
+        {
             float deltaTime = Raylib.GetFrameTime();
 
-            // Pallon aloituspaikka on ruudun keskellä
-            Vector2 ballPosition = Raylib.GetScreenCenter();
-
-            // Pallon suunta normalisoidaan että sen pituudeksi tulee 1
-            Vector2 ballDirection = Vector2.Normalize(new Vector2(1, 0.5f));
-
-            // Pallon nopeuden yksikkö on pikseleitä sekunnissa
-            float ballSpeed = 160;
-
-            while (Raylib.WindowShouldClose() == false)
+            // Pelaaja 1 (W/S)
+            if (Raylib.IsKeyDown(KeyboardKey.W))
             {
-                Raylib.BeginDrawing();
-                Raylib.ClearBackground(Color.Black);
+                player1.Y -= playerSpeed * deltaTime;
+            }
 
-                // Piirrä vasemman puoleinen maila.
-                Raylib.DrawRectangleV(player1, new Vector2(20, 100), Color.White);
+            if (Raylib.IsKeyDown(KeyboardKey.S))
+            {
+                player1.Y += playerSpeed * deltaTime;
+            }
 
+            // Pelaaja 2 (Nuolinäppäimet)
+            if (Raylib.IsKeyDown(KeyboardKey.Up))
+            {
+                player2.Y -= playerSpeed * deltaTime;
+            }
 
-                //Piitträ oikean puoleinen maila.
-                Raylib.DrawRectangleV(player2, new Vector2(20, 100), Color.Red);
+            if (Raylib.IsKeyDown(KeyboardKey.Down))
+            {
+                player2.Y += playerSpeed * deltaTime;
+            }
 
+            // Estä kentän ulkopuolelle meneminen
+            ClampPlayers();
+        }
 
-                //liikuta mailoja ylös ja alas.
-                if (Raylib.IsKeyDown(KeyboardKey.Up))
-                {
-                    if (IsTouchingUp(player2) == false)
-                    {
-                        player2.Y = player2.Y - speed * Raylib.GetFrameTime();
-                    }
-                }
-                else if (Raylib.IsKeyDown(KeyboardKey.Down))
-                {
-                    if (IsTouchingDown(player2) == false)
-                    {
-                        player2.Y = player2.Y + speed * Raylib.GetFrameTime();
-                    }
-                }
+        void ClampPlayers()
+        {
+            // Pelaaja 1
+            if (player1.Y < 0)
+            {
+                player1.Y = 0;
+            }
+            else if (player1.Y + playerSize.Y > screenHeight)
+            {
+                player1.Y = screenHeight - playerSize.Y;
+            }
 
-                if (Raylib.IsKeyDown(KeyboardKey.W))
-                {
-                    if (IsTouchingUp(player1) == false)
-                    {
-                        player1.Y = player1.Y - speed * Raylib.GetFrameTime();
-                    }
-                }
-                else if (Raylib.IsKeyDown(KeyboardKey.S))
-                {
-                    if (IsTouchingDown(player1) == false)
-                    {
-                        player1.Y = player1.Y + speed * Raylib.GetFrameTime();
-                    }
-                }
-
-
-                Raylib.DrawCircleV(ballPosition, 20 , Color.White);
-
-                Raylib.EndDrawing();
-
-
-                ballPosition = ballPosition + ballDirection * ballSpeed * Raylib.GetFrameTime();
+            // Pelaaja 2
+            if (player2.Y < 0)
+            {
+                player2.Y = 0;
+            }
+            else if (player2.Y + playerSize.Y > screenHeight)
+            {
+                player2.Y = screenHeight - playerSize.Y;
             }
         }
-        static bool IsTouchingSomething(Vector2 v)
+
+        void MoveBall()
         {
-            if (IsTouchingUp(v))
+            ballPosition += ballDirection * ballSpeed * Raylib.GetFrameTime();
+        }
+
+        void CheckBallCollisions()
+        {
+            // Ylä- ja alareuna
+            if (ballPosition.Y <= 0 || ballPosition.Y >= screenHeight)
             {
-                return true;
+                ballDirection.Y *= -1;
             }
-            else
+
+            Rectangle player1Rectangle = new Rectangle(player1, playerSize);
+            Rectangle player2Rectangle = new Rectangle(player2, playerSize);
+
+            // Törmäys pelaajaan 1
+            if (Raylib.CheckCollisionPointRec(ballPosition, player1Rectangle))
             {
-                return false;
+                ballDirection.X *= -1;
+
+                // Työnnä pallo ulos mailasta
+                ballPosition.X = player1.X + playerSize.X;
+            }
+
+            // Törmäys pelaajaan 2
+            if (Raylib.CheckCollisionPointRec(ballPosition, player2Rectangle))
+            {
+                ballDirection.X *= -1;
+
+                // Työnnä pallo ulos mailasta
+                ballPosition.X = player2.X;
+            }
+
+            // Vasen reuna -> pelaaja 2 saa pisteen
+            if (ballPosition.X < 0)
+            {
+                player2Score++;
+                ResetBall();
+            }
+
+            // Oikea reuna -> pelaaja 1 saa pisteen
+            if (ballPosition.X > screenWidth)
+            {
+                player1Score++;
+                ResetBall();
             }
         }
-        static bool IsTouchingUp(Vector2 v)
+
+        void Draw()
         {
-            if (v.Y > 0)
-            { 
-                return false;
-            }
-            else
-            {
-                return true; 
-            }
-        }
-        static bool IsTouchingDown(Vector2 v)
-        {
-            if( (v.Y + 100 ) > Raylib.GetScreenHeight())
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
+            Raylib.BeginDrawing();
+
+            Raylib.ClearBackground(Color.Black);
+
+            // Pelaajat
+            Raylib.DrawRectangleV(player1, playerSize, Color.White);
+            Raylib.DrawRectangleV(player2, playerSize, Color.Red);
+
+            // Pallo
+            Raylib.DrawCircleV(ballPosition, 10, Color.White);
+
+            // Pisteet
+            Raylib.DrawText(
+                player1Score.ToString(),
+                screenWidth / 4,
+                20,
+                40,
+                Color.White
+            );
+
+            Raylib.DrawText(
+                player2Score.ToString(),
+                screenWidth * 3 / 4,
+                20,
+                40,
+                Color.White
+            );
+
+            Raylib.EndDrawing();
         }
     }
 }
